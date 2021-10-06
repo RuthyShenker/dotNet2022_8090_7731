7828f3eea22568f942a2cd0bb7f15ad9a30bf9e6
RuthyShenker <s6161890@gmail.com>
2021-10-06 15:47:54 +0300

    Initial code + Welcome functions

---
+++ dotNet2022_8090_7731/Targil0/Program8090.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome8090();
+            Welcome7731();
+            Console.ReadKey();
+        }
+        static partial void Welcome7731();
+        private static void Welcome8090()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
