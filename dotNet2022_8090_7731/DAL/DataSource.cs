ded7a1b7fc4cfe7d2a44cd1e3186be76843a5a0d
RuthyShenker <s6161890@gmail.com>
2021-10-11 18:20:39 +0300

    Adding functions

--- dotNet2022_8090_7731/DAL/DataSource.cs
+++ dotNet2022_8090_7731/DAL/DataSource.cs
@@ -4,10 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DAL.DO;
+
+
 namespace DalObject
 {
+
     class DataSource
     {
+        public static Random rand = new Random();
+
         static internal Drone[] DroneArr = new Drone[10];
         static internal BasicStation[] BasicStationArr = new BasicStation[5];
         static internal Customer[] CustomerArr = new Customer[100];
@@ -21,6 +26,10 @@ namespace DalObject
             internal static int IndexCustomerArr;
             internal static int IndexParcelArr;
             //there is another field!!
+        }
+        public static void Initialize()
+        {
+            DroneArr[ rand.Next(DroneArr.Length)]=(64964,h9h);
         }
     }
 }
