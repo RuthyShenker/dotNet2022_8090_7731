3e89d23f2c6e2cbc9a6d8ef7765d23bb125dd7b6
ChaniBroner <chbr731@gmail.com>
2021-10-11 17:01:44 +0300

    add struct Drone & add struct Customer

---
+++ dotNet2022_8090_7731/DAL/Drone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    namespace DO
+    {
+        public struct Drone
+        {
+            public string Id { get; set; }
+            public string Model { get; set; }
+            public WeightCategories MaxWeight { get; set; }
+            public double BatteryStatus { get; set; }
+            public DroneStatuses Status { get; set; }
+
+        }
+    }
+}
