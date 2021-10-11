3d8694015cdaf7cb8e05a56a56e2f546cf3dee65
RuthyShenker <s6161890@gmail.com>
2021-10-11 17:04:41 +0300

    adding ChargingDrone and DataSource structs

---
+++ dotNet2022_8090_7731/DAL/ChargingDrone.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DAL
+{
+    namespace DO
+    {
+        public struct ChargingDrone
+        {
+            public int StationId{ get; set; }
+            public int DroneId{ get; set; }
+        }
+    }
+}
