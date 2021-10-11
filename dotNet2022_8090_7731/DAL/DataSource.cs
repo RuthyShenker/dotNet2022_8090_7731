0c86721b2c6638e06ad736a6144b9b845567e8a5
ChaniBroner <chbr731@gmail.com>
2021-10-11 17:28:53 +0300

    add fields to class DataSource-DronerArr,BasicStationArr,CustomerArr,ParcelArr and add fileds to config class

--- dotNet2022_8090_7731/DAL/DataSource.cs
+++ dotNet2022_8090_7731/DAL/DataSource.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using DAL.DO;
 namespace DalObject
 {
     class DataSource
     {
-        class Config
-        {
+        static internal Drone[] DroneArr = new Drone[10];
+        static internal BasicStation[] BasicStationArr = new BasicStation[5];
+        static internal Customer[] CustomerArr = new Customer[100];
+        static internal Parcel[] ParcelArr = new Parcel[1000];
 
+
+        internal class Config
+        {
+            internal static int IndexDroneArr;
+            internal static int IndexBasicStationArr;
+            internal static int IndexCustomerArr;
+            internal static int IndexParcelArr;
+            //there is another field!!
         }
     }
 }
