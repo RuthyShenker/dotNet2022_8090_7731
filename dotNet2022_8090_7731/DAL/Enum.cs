586de529a3febe5a3edbc31344a107ddfb674d67
RuthyShenker <s6161890@gmail.com>
2021-10-11 17:05:20 +0300

    Adding Enum and Parcel structs

---
+++ dotNet2022_8090_7731/DAL/Enum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    namespace DO
+    {
+        public enum WeightCategories { light, medium, heavy };
+        public enum UrgencyStatuses { normal, fast, emergency };
+        public enum DroneStatuses { available, maintenance, delivery };
+    };
+}
---
+++ dotNet2022_8090_7731/DAL/Parcel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    namespace DO
+    {
+        public struct Parcel
+        {
+            public string ParcelId { get; set; }
+            public int SenderId { get; set; }
+            public int GetterId { get; set; }
+            public WeightCategories Weight { get; set; }
+            public UrgencyStatuses Status { get; set; }
+            public int DroneId { get; set; }
+            public DateTime MakingParcel { get; set; }
+            public DateTime PickingUp { get; set; }
+            public DateTime Arrival { get; set; }
+            public DateTime MatchingParcel { get; set; }
+        }
+    }
+}
