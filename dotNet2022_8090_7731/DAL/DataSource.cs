dc93c3f56d136f39d5862faf4167d79641dd17a6
RuthyShenker <s6161890@gmail.com>
2021-10-13 19:03:49 +0300

    Start To Initialize

--- dotNet2022_8090_7731/DAL/DataSource.cs
+++ dotNet2022_8090_7731/DAL/DataSource.cs
@@ -29,7 +29,38 @@ namespace DalObject
         }
         public static void Initialize()
         {
-            DroneArr[ rand.Next(DroneArr.Length)]=(64964,h9h);
-        }
+            const int INITIALIZE_DRONE = 5;
+            Drone fillDrone;
+            for (int i = 0; i < INITIALIZE_DRONE; i++)
+            {
+                fillDrone = DroneArr[Config.IndexDroneArr];
+                fillDrone.Id = rand.Next(100000000, 999999999).ToString();
+                fillDrone.Model = rand.Next(1000, 9999).ToString();
+                fillDrone.MaxWeight = (WeightCategories)(rand.Next(0, Enum.GetNames(typeof(WeightCategories)).Length));
+                fillDrone.BatteryStatus = rand.Next(0, 100);
+                fillDrone.Status = (DroneStatuses)(rand.Next(0, Enum.GetNames(typeof(DroneStatuses)).Length));
+                Config.IndexDroneArr++;
+            }
+
+
+            const int INITIALIZE_CUSTOMER = 10;
+            string[] nameForInitialize = {"UriA","Aviad","Ariel",};
+            Customer fillCustomer;
+            for (int i = 0; i < INITIALIZE_CUSTOMER; i++)
+            {
+                fillCustomer = CustomerArr[Config.IndexCustomerArr];
+                fillCustomer.Id = rand.Next(100000000, 999999999).ToString();
+                fillCustomer.Model = rand.Next(1000, 9999).ToString();
+                fillCustomer.MaxWeight = (WeightCategories)(rand.Next(0, Enum.GetNames(typeof(WeightCategories)).Length));
+                fillCustomer.BatteryStatus = rand.Next(0, 100);
+                fillCustomer.Status = (DroneStatuses)(rand.Next(0, Enum.GetNames(typeof(DroneStatuses)).Length));
+            }
+
+             public string Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+    }
     }
 }
