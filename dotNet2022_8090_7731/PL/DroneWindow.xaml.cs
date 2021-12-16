4ea74eecad8710b16e29801eaf59c36903fc7019
ChaniBroner <91884898+ChaniBroner@users.noreply.github.com>
2021-12-16 17:05:00 +0200

    -correct merge-

--- dotNet2022_8090_7731/PL/DroneWindow.xaml.cs
+++ dotNet2022_8090_7731/PL/DroneWindow.xaml.cs
@@ -10,11 +10,8 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Text.RegularExpressions;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using System.Windows.Controls.Primitives;
-using System.Windows.Threading;
 
 namespace PL
 {
@@ -25,92 +22,103 @@ namespace PL
     {
 
         private IBL.IBL bl;
+        Action refreshDroneList;
 
-        public DroneWindow(IBL.IBL bl)
+        public DroneWindow(IBL.IBL bl, Action initializeDrones)
         {
+            this.bl = bl;
+            refreshDroneList = initializeDrones;
+
             InitializeComponent();
             GridOfAddDrone.Visibility = Visibility.Visible;
             GridOfUpdateDrone.Visibility = Visibility.Collapsed;
             detailsOfDrone.DataContext = new DroneToList();
         }
-        public DroneWindow(IBL.IBL bl, DroneToList selectedDrone)
-        {
+        public DroneWindow(IBL.IBL bl, Action initializeDrones, DroneToList selectedDrone)
+        {            
+            this.bl = bl;
+            refreshDroneList = initializeDrones;
+
             InitializeComponent();
             
             detailsOfDrone.DataContext = selectedDrone;
             EnableOfTextbox();
             GridOfAddDrone.Visibility = Visibility.Collapsed;
             GridOfUpdateDrone.Visibility = Visibility.Visible;
+            if (selectedDrone.DStatus == DroneStatus.Delivery)
+                SendOrReleaseDroneFromCharging.Visibility = Visibility.Collapsed;
+            else if (selectedDrone.DStatus == DroneStatus.Maintenance)
+                SendOrReleaseDroneFromCharging.DataContext = "ReleaseDronefromCharge:";
+            else SendOrReleaseDroneFromCharging.DataContext = "SendDroneToCharge";
+        }
+
+
+        private void EnableOfTextbox()
+        {
+            IdTextBox.IsEnabled = false;
+            BatteryTextBox.IsEnabled = false;
+            MaxWeightComboBox.IsEnabled = false;
+            StatusComboBox.IsEnabled = false;
+            DeliveryTextBox.IsEnabled = false;
+            LatitudeTextBox.IsEnabled = false;
+            LongitudeTextBox.IsEnabled = false;
         }
 
         private void Button_Click_Of_Adding_New_Drone(object sender, RoutedEventArgs e)
         {
 
-            CheckValidDrone((DroneToList)detailsOfDrone.DataContext, (Button)sender);
-
-            //int id;
-            //int.TryParse(IdTextBox.Text, out id);
-            //float batteryStatus;
-            //float.TryParse(BatteryTextBox.Text, out batteryStatus);
-            //string model = ModelTextBox.Text;
-            //double longitude;
-            //double latitude;
-            //double.TryParse(LongitudeTextBox.Text, out longitude);
-            //double.TryParse(LatitudeTextBox.Text, out latitude);
-            //WeightCategories weight = (WeightCategories)MaxWeightComboBox.SelectedIndex;
-            //DroneStatus droneStatus = (DroneStatus)StatusComboBox.SelectedIndex;
-            //Drone newDrone = new Drone(id, model, weight, batteryStatus, droneStatus, null, new Location(longitude, latitude));
-            ////bl.AddingDrone(newDrone, 0);  
-
+            CheckValidDrone((DroneToList)detailsOfDrone.DataContext);
+ 
+            int id;
+            int.TryParse(IdTextBox.Text, out id);
+            float batteryStatus;
+            float.TryParse(BatteryTextBox.Text, out batteryStatus);
+            string model = ModelTextBox.Text;
+            double longitude;
+            double latitude;
+            double.TryParse(LongitudeTextBox.Text, out longitude);
+            double.TryParse(LatitudeTextBox.Text, out latitude);
+            WeightCategories weight = (WeightCategories)MaxWeightComboBox.SelectedIndex;
+            DroneStatus droneStatus = (DroneStatus)StatusComboBox.SelectedIndex;
+            Drone newDrone = new Drone(id, model, weight, batteryStatus, droneStatus, null, new Location(longitude, latitude));
+            //bl.AddingDrone(newDrone, 0);  
+            
             ///תחנה להטענה
             ///delivery?
         }
 
-        private void CheckValidDrone(DroneToList drone, Button sender)
+        private void CheckValidDrone(DroneToList drone)
         {
-
-            MessageBox.Show($"{drone.Model},{drone.CurrLocation.Longitude } {drone.CurrLocation}, {drone.CurrLocation.Latitude}");
-            bool isExist = bl.GetDrones().Any(d => d.Id == drone.Id);
-            if (isExist)
+            bool check = bl.GetDrones().Any(d => d.Id == drone.Id);
+            if (check == true)
             {
-                var textBox = (TextBox)message.FindName("IdTextBox");
-                AddToolTip(textBox, " Id is not available ");
-                return;
+                TextBlock TextBlock = new TextBlock();
+                TextBlock.Text= "Id is already exist";
+                message.Children.Add(TextBlock);
+            }
+            if (drone.BatteryStatus<0 ||drone.BatteryStatus>100)
+            {
+                TextBlock TextBlock = new TextBlock();
+                if(drone.BatteryStatus < 0)
+                    TextBlock.Text = "Battery Status is less than zero";
+                else TextBlock.Text = "Battery Status is more than hundred" ;
+
+                message.Children.Add(TextBlock);
+            }
+            if (!drone.Model.All(ch=>char.IsNumber(ch)))
+            {
+                TextBlock TextBlock = new TextBlock();
+                TextBlock.Text = "Id has to contain only numbers";
+                 message.Children.Add(TextBlock);
             }
 
-            if (drone.CurrLocation.Longitude is < (-90) or > 90)
-            {
-                var textBox = (TextBox)message.FindName("Longitude");
-                AddToolTip(textBox, "InCorrect longitude (-90, 90) ");
-                return;
-            }
-
-            if (drone.CurrLocation.Latitude is < (-90) or > 90)
-            {
-                var textBox = (TextBox)message.FindName("Latitude");
-                AddToolTip(textBox, "InCorrect Latitude (-90, 90) ");
-                return;
-            }
-
-
-
-            // to finish
         }
 
-        private void AddToolTip(TextBox textBox, string str)
+        private void Close_Drone_Window_Click(object sender, RoutedEventArgs e)
         {
-            ToolTip toolTip = new ToolTip();
-            toolTip.Content = str;
-            toolTip.IsOpen = true;
-            textBox.ToolTip = toolTip;
+            this.Close();
+        }
 
-            // Turn off ToolTip
-            DispatcherTimer timer = new DispatcherTimer { Interval = new TimeSpan(0, 0, 1), IsEnabled = true };
-            timer.Tick += new EventHandler(delegate (object timerSender, EventArgs timerArgs)
-            {
-                toolTip.IsOpen = false;
-                timer = null;
-            });
         private void Update_Model_Click(object sender, RoutedEventArgs e)
         {
             DroneToList drone= detailsOfDrone.DataContext as DroneToList;
@@ -125,51 +133,22 @@ namespace PL
             refreshDroneList();
         }
 
-        void TextBox_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        private void Send_Or_Release_Drone_From_Charging(object sender, RoutedEventArgs e)
         {
-            var textBox = (TextBox)sender;
-            if (e.Handled = new Regex("[^0-9]+").IsMatch(e.Text))
+            DroneToList drone=detailsOfDrone.DataContext as DroneToList;
+
+            if (drone.DStatus == DroneStatus.Free)
             {
-                textBox.Background = Brushes.Gray;
-                ToolTip toolTip = new ToolTip();
-                AddToolTip(textBox, " Input has to contain only digits ");
+                bl.SendingDroneToCharge(drone.Id);
+                SendOrReleaseDroneFromCharging.DataContext = "ReleaseDronefromCharge:";
             }
             else
             {
-                textBox.Background = Brushes.White;
+                bl.ReleasingDrone(drone.Id);
+                SendOrReleaseDroneFromCharging.DataContext = "SendDroneToCharge:";
             }
         }
-        //private void IdTextBox_TextChanged(object sender, TextChangedEventArgs e)
-        //{
-        //    if (!IdTextBox.Text.All(ch => char.IsNumber(ch)))
-        //    {
-        //        TextBox textBox = (TextBox)detailsOfDrone.FindName("IdTextBox");
-        //        textBox.BorderBrush = Brushes.Red;
-        //    }
-        //}
-
-        private void MaxWeightComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-
-
-        //        IdTextBox
-        //BatteryTextBox
-        //MaxWeightComboBox
-        //ModelTextBox
-        //StatusComboBox
-        //DeliveryTextBox
-        //LatitudeTextBox
-        //LongitudeTextBox
-
-        //private void Close_Drone_Window_Click(object sender, RoutedEventArgs e)
-        //{
-        //    this.Close();
-        //}
     }
 }
-
 
 
