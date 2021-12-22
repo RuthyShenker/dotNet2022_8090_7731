6aad07b5ab5dc336e8c6b13aa64055555d40d47d
RuthyShenker <s6161890@gmail.com>
2021-12-22 18:46:14 +0200

    material design

---
+++ dotNet2022_8090_7731/PL/Converters/IsDigitConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace PL.Converters
+{
+    public class IsDigitConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string && (value as string).All(d => char.IsDigit(d) == true))
+            {
+
+                return "k";
+            }
+            else
+            {
+                return "Must contain digits only";
+            }
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
+
