79c6cf1bc1d7722040336b6d66610eccaf64f7b0
ChaniBroner <chbr731@gmail.com>
2021-12-22 22:47:20 +0200

    continue

--- dotNet2022_8090_7731/PL/MainWindow.xaml.cs
+++ dotNet2022_8090_7731/PL/MainWindow.xaml.cs
@@ -26,8 +26,7 @@ namespace PL
         {
             bl = BlApi.BlFactory.GetBl();
             InitializeComponent();
-        TabItem t=new TabItem();
-        T.C
+
         }
         private void btn_DroneListView_Click(object sender, RoutedEventArgs e)
         {
