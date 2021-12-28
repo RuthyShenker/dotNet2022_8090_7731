5a45580971838bb72a8f9cc9510e67e2a6b640af
RuthyShenker <s6161890@gmail.com>
2021-12-28 18:21:30 +0200

    try generic po

---
+++ dotNet2022_8090_7731/PL/try.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL
+{
+    class Tryial<T>
+    {
+
+        public object Id
+        {
+            get
+            {
+                var a = typeof(T).GetProperty("id");
+                return a;
+            }
+
+            set
+            {
+                var a = typeof(T).GetProperty("id");
+                a.SetValue(a, value);
+                //RaisePropertyChanged("Id");
+                //validityMessages["Id"] = IdMessage(value, ID_LENGTH);
+            }
+        }
+
+    }
+}
