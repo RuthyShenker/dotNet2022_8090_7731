97ab29d98590de2de1cc7e47609740e088914163
RuthyShenker <s6161890@gmail.com>
2022-02-14 03:20:31 +0200

    continue simulator logic

--- dotNet2022_8090_7731/BL/BL/Simulator.cs
+++ dotNet2022_8090_7731/BL/BL/Simulator.cs
@@ -60,38 +60,42 @@ namespace BL
 
         private void TryAssumingParcel(Drone drone)
         {
-            if (!SleepDelayTime()) return;
-
             lock (bl)
             {
                 var optionalParcels = bl.OptionalParcelsForSpecificDrone(drone.BatteryStatus, drone.Weight, drone.CurrLocation);
+
                 if (optionalParcels.Any())
                 {
+                    if (!SleepDelayTime()) return;
                     var parcel = optionalParcels.First();
 
                     switch (parcel.Id, drone.BatteryStatus)
                     {
                         // if there is no parcel to assign and drone's battery is full.  
                         case (0, 1.0):
+                            do
+                            {
+                                SleepDelayTime(); // TODO if?
+                                optionalParcels = bl.OptionalParcelsForSpecificDrone(drone.BatteryStatus, drone.Weight, drone.CurrLocation);
+                            } while (optionalParcels == null);
                             break;
 
                         // if the is no parcel to assign and the the drone's battery is not full.
                         case (0, _):
                             station = bl.ClosestStation(drone.CurrLocation, true);
-                            stationId = station.Id;
 
-                            if (stationId != default)
+                            if (station.Id != default)
                             {
                                 drone.DroneStatus = DroneStatus.Maintenance;
                                 maintenance = Maintenance.Assigning;
-                                dal.Update<DO.BaseStation>(stationId, station.NumAvailablePositions - 1, nameof(station.NumAvailablePositions));
-                                dal.Add(new DO.ChargingDrone(drone.Id, stationId, DateTime.Now));
+                                dal.Update<DO.BaseStation>(station.Id, station.NumAvailablePositions - 1, nameof(station.NumAvailablePositions));
+                                dal.Add(new DO.ChargingDrone(drone.Id, station.Id, DateTime.Now));
                             }
                             break;
 
                         // if there is parcel to assign and there is enough battery.
                         case (_, _):
-                            init(parcel.Id);
+                            Init(parcel.Id);
                             drone.DroneStatus = DroneStatus.Delivery;
 
                             dal.Update<DO.Parcel>(parcel.Id, DateTime.Now, nameof(DO.Parcel.BelongParcel));
@@ -100,7 +104,6 @@ namespace BL
 
                         default:
                     }
-
                 }
             }
         }
@@ -121,102 +124,105 @@ namespace BL
         //};
         private void CompleteCharging(Drone drone)
         {
-            switch (maintenance)
+            while (drone.DroneStatus == DroneStatus.Maintenance && !checkStop())
             {
-                //TODO what happens when there is no available station
-                case Maintenance.Assigning:
-                    station = bl.ClosestStation(drone.CurrLocation, true);
-                    distance = Extensions.CalculateDistance(drone.CurrLocation, station.Location);
-                    maintenance = Maintenance.GoingTowardStation;
-                    break;
+                switch (maintenance)
+                {
+                    //TODO what happens when there is no available station
+                    case Maintenance.Assigning:
+                        station = bl.ClosestStation(drone.CurrLocation, true);
+                        distance = Extensions.CalculateDistance(drone.CurrLocation, station.Location);
+                        maintenance = Maintenance.GoingTowardStation;
+                        break;
 
-                case Maintenance.GoingTowardStation:
-                    //lock (bl)
-                    {
-                        while (distance > 0.01 && !checkStop())
+                    case Maintenance.GoingTowardStation:
+                        //lock (bl)
+                        {
+                            while (distance > 0.01 && !checkStop())
+                            {
+                                if (!SleepDelayTime()) break;
+                                double delta = distance < STEP ? distance : STEP;
+                                distance -= delta;
+                                drone.BatteryStatus = Math.Max(0.0, drone.BatteryStatus - delta * bl.BatteryUsages[DRONE_FREE]);
+                                updateView();
+                            }
+                            if (distance <= 0.01)
+                                lock (bl)
+                                {
+                                    drone.CurrLocation = station.Location;
+                                    maintenance = Maintenance.Charging;
+                                }
+                        }
+                        break;
+
+                    case Maintenance.Charging:
+                        while (drone.BatteryStatus < 1.0 && !checkStop())
                         {
                             if (!SleepDelayTime()) break;
-                            double delta = distance < STEP ? distance : STEP;
-                            distance -= delta;
-                            drone.BatteryStatus = Math.Max(0.0, drone.BatteryStatus - delta * bl.BatteryUsages[DRONE_FREE]);
-                            updateView();
-                        }
-                        if (distance <= 0.01)
-                            lock (bl)
+                            //lock (bl) למה בפרויקט לדוג עשו כאן?
                             {
-                                drone.CurrLocation = station.Location;
-                                maintenance = Maintenance.Charging;
+                                drone.BatteryStatus = Math.Min(1.0, drone.BatteryStatus + chargingRate * TIME_STEP);
+                                updateView();
                             }
-                    }
-                    break;
-
-                case Maintenance.Charging:
-                    while (drone.BatteryStatus < 1.0 && !checkStop())
-                    {
-                        if (!SleepDelayTime()) break;
-                        //lock (bl) למה בפרויקט לדוג עשו כאן?
-                        {
-                            drone.BatteryStatus = Math.Min(1.0, drone.BatteryStatus + chargingRate * TIME_STEP);
-                            updateView();
                         }
-                    }
-                    if (drone.BatteryStatus >= 1.0)
-                    {
-                        drone.DroneStatus = DroneStatus.Free;
-                    }
-                    break;
-                default:
-                    break;
+                        if (drone.BatteryStatus >= 1.0)
+                        {
+                            drone.DroneStatus = DroneStatus.Free;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                updateView();
             }
         }
 
-        private void init(int parcelId)
+        private void Init(int parcelId)
         {
             parcel = dal.GetFromDalById<DO.Parcel>(parcelId);
             pickedUp = parcel.PickingUp.HasValue;
             customer = bl.GetCustomer(pickedUp ? parcel.GetterId : parcel.SenderId);
+            distance = Extensions.CalculateDistance(drone.CurrLocation, customer.Location);
 
             batteryUsage = (int)Enum.Parse(typeof(BatteryUsage), parcel?.Weight.ToString());
         }
 
         private void CompleteDelivery(Drone drone)
         {
-            lock (bl)
-            {
-                if (parcel.Equals(default)) init(drone.PInTransfer.PId); 
-                distance = Extensions.CalculateDistance(drone.CurrLocation, customer.Location);
-            }
+            if (parcel.Equals(default)) Init(drone.PInTransfer.PId);
 
-            if (distance < 0.01 || drone.BatteryStatus == 0.0)
-                lock (bl)
-                {
-                    drone.CurrLocation = customer.Location;
-                    if (pickedUp)
-                    {
-                        dal.ParcelDelivery((int)parcel?.Id);
-                        drone.Status = DroneStatuses.Available;
-                    }
-                    else
-                    {
-                        dal.ParcelPickup((int)parcel?.Id);
-                        customer = bl.GetCustomer((int)parcel?.TargetId);
-                        pickedUp = true;
-                    }
-                }
-            else
+            while (distance > 0.01 && drone.BatteryStatus != 0 && !checkStop())
             {
-                if (!sleepDelayTime()) break;
+                if (!SleepDelayTime()) break;
                 lock (bl)
                 {
                     double delta = distance < STEP ? distance : STEP;
                     double proportion = delta / distance;
-                    drone.Battery = Max(0.0, drone.Battery - delta * bl.BatteryUsages[pickedUp ? batteryUsage : DRONE_FREE]);
-                    double lat = drone.Location.Latitude + (customer.Location.Latitude - drone.Location.Latitude) * proportion;
-                    double lon = drone.Location.Longitude + (customer.Location.Longitude - drone.Location.Longitude) * proportion;
-                    drone.Location = new() { Latitude = lat, Longitude = lon };
+                    drone.BatteryStatus = Math.Max(0.0, drone.BatteryStatus - delta * bl.BatteryUsages[pickedUp ? batteryUsage : DRONE_FREE]);
+                    double lat = drone.CurrLocation.Latitude + (customer.Location.Latitude - drone.CurrLocation.Latitude) * proportion;
+                    double lon = drone.CurrLocation.Longitude + (customer.Location.Longitude - drone.CurrLocation.Longitude) * proportion;
+                    drone.CurrLocation = new() { Latitude = lat, Longitude = lon };
+
+                    distance = Extensions.CalculateDistance(drone.CurrLocation, customer.Location);
+                    updateView();
                 }
             }
-            break;
+            if ((distance <= 0.01 || drone.BatteryStatus == 0.0) && !checkStop())
+            {
+                drone.CurrLocation = customer.Location;
+                if (pickedUp)
+                {
+                    dal.Update<DO.Parcel>(parcel.Id, DateTime.Now, nameof(parcel.Arrival));
+                    drone.DroneStatus = DroneStatus.Free;
+                }
+                else
+                {
+                    dal.Update<DO.Parcel>(parcel.Id, DateTime.Now, nameof(parcel.PickingUp));
+
+                    customer = bl.GetCustomer(parcel.GetterId);
+                    pickedUp = true;
+                }
+            }
         }
 
         private static bool SleepDelayTime()
